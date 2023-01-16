cc17bffbf595d99269458df7145f176b34e51b7b
Michael Mason <TeaUponTweed@gmail.com>
2023-01-16 16:58:28 -0700

    remove article content for now since there are too many edge cases

--- src/yesterdays_hackernews/app.py
+++ src/yesterdays_hackernews/app.py
@@ -3,9 +3,11 @@ import datetime
 import functools
 import os
 import pathlib
+from datetime import datetime, timedelta, timezone
 
 import markdown
-import pynliner
+import prefect
+import pytz
 import requests
 import trafilatura
 from bs4 import BeautifulSoup
@@ -14,9 +16,19 @@ from jinja2 import Environment, FileSystemLoader
 from yesterdays_hackernews.send import send_mesage
 
 
+def hours_since_8am_mt():
+    mt = pytz.timezone("US/Mountain")
+    now = datetime.now(mt)
+    eight_am_today = now.replace(hour=8, minute=0, second=0, microsecond=0)
+    if now.hour < 8:
+        eight_am_today = eight_am_today - datetime.timedelta(days=1)
+    hours_passed = (now - eight_am_today).total_seconds() / 3600
+    return round(hours_passed)
+
+
 def get_yesterday() -> str:
-    today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=1)
+    today = datetime.now()
+    yesterday = today - timedelta(days=1)
     return yesterday.strftime("%Y-%m-%d")
 
 
@@ -34,18 +46,9 @@ def get_markdown(url: str) -> str:
 
 def get_articles_links_and_title(response: requests.Response) -> tuple[str, str]:
     soup = BeautifulSoup(response.text, "html.parser")
-    title_lines = soup.find_all("span", class_="titleline")[:10]
+    title_lines = soup.find_all("span", class_="titleline")
     links = [line.find("a") for line in title_lines]
-    return [(link["href"], link.string) for link in links]
-
-
-def hours_since_8AM():
-    now = datetime.datetime.now()
-    eight_am_today = now.replace(hour=8, minute=0, second=0, microsecond=0)
-    if now.hour < 8:
-        eight_am_today = eight_am_today - datetime.timedelta(days=1)
-    hours_passed = (now - eight_am_today).total_seconds() / 3600
-    return round(hours_passed)
+    return [(link["href"], link.string) for link in links if link['href'].startswith('http')][:14]
 
 
 @functools.lru_cache(maxsize=1)
@@ -53,12 +56,7 @@ def get_yesterdays_top_ten(yesterday) -> list[tuple[str, str]]:
     url = "https://news.ycombinator.com/front"
     response = requests.get(url, params={"day": yesterday})
     links_and_title = get_articles_links_and_title(response)
-    assert len(links_and_title) == 10
     return links_and_title
-
-
-def inline_css(html_string: str, css_string: str) -> str:
-    return
 
 
 def apply_template(template_name: str, context: dict) -> str:
@@ -66,27 +64,44 @@ def apply_template(template_name: str, context: dict) -> str:
     template = env.get_template(template_name)
     return template.render(context)
 
+def get_email_html(link, title):
+    # markdown_text = get_markdown(link)
+    # extensions = [
+    #     "markdown.extensions.fenced_code",
+    #     "markdown.extensions.tables",
+    #     "markdown.extensions.sane_lists",
+    # ]
+    # html_output = markdown.markdown(markdown_text, extensions=extensions)
+    html_output = apply_template(
+        "email_template.html",
+        {"article_title": title, "article_url": link}#, "article_body": html_output},
+    )
+    return html_output
 
-def pipeline():
+def get_emails_to_send() -> list[str]:
     yesterday = get_yesterday()
     articles_links_and_title = get_yesterdays_top_ten(yesterday)
-    ix = hours_since_8AM()
-    if 0 <= ix < 10:
-        link, title = articles_links_and_title[ix]
-        markdown_text = get_markdown(link)
-        extensions = [
-            "markdown.extensions.fenced_code",
-            "markdown.extensions.tables",
-            "markdown.extensions.sane_lists",
-        ]
-        html_output = markdown.markdown(markdown_text, extensions=extensions)
-        html_output = apply_template(
-            "email_template.html",
-            {"article_title": title, "article_url": link, "article_body": html_output},
-        )
-        print(html_output, file=open("test.html", "w"))
-        send_mesage("teaupontweed@gmail.com", f"Hacker News: '{title}'", html_output)
+    print(f"Got {len(articles_links_and_title )} artciles for {yesterday}")
+    ix = hours_since_8am_mt()
+    if 0 <= ix < len(articles_links_and_title) - 1 and ix % 2 == 0:
+        url1, title1 = articles_links_and_title[ix]
+        em1 = get_email_html(url1, title1)
+        url2, title2 = articles_links_and_title[ix+1]
+        em2 = get_email_html(url2, title2)
+        return [(title1,em1), (title2,em2)]
+    else:
+        return []
+
+@prefect.task
+def pipeline():
+    emails = get_emails_to_send()
+    for title, html_output in emails:
+        for email in os.environ["NEWS_EMAILS"].split():
+            send_mesage(email, f"Hacker News: '{title}'", html_output)
 
 
 if __name__ == "__main__":
-    pipeline()
+    schedule = prefect.schedules.CronSchedule(cron="5 * * * *")
+    with prefect.Flow("yesterdays_hackernews", schedule=schedule) as flow:
+        pipeline()
+    flow.run()
